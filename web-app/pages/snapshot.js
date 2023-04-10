8801c52f2224422a58d7d6794494832f06460c9c
aswinraj94 <aswinraj94@gmail.com>
2023-04-10 15:24:52 +0100

    Modified the boilerplate to include Iframe

---
+++ web-app/pages/snapshot.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffects} from 'react';
+import styles from "../styles/Home.module.css";
+import Link from 'next/link'
+function App() {
+
+  return (
+
+
+    
+    <div >
+
+    <div className="App">
+      <iframe seamless src="https://snapshot.org/#/" width={2030} height={1000} allow="fullscreen" ></iframe>
+    </div>
+    </div>
+  );
+}
+
+export default App;
